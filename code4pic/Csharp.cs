41aae34c950ea10c6d5b309f1154b2c05308ed16
saki <sakinijino@gmail.com>
2011-03-31 15:03:08 +0800

    add online syntax highlighter tool.

--- code4pic/Csharp.cs
+++
@@ -1,9 +0,0 @@
-private void SelectedImage_ImageOpened(object sender, RoutedEventArgs e)
-{
-  if (!StopSlideshow) {
-    ShowMainImage.Begin(); 
-  } else { 
-    ShowForSelected.Begin();
-  } 
-} 
-
